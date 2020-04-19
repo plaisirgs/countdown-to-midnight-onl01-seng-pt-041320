7b417166a7f6c697d18c117b53442ccb33dd5aa9
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-04-19 01:57:16 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
-#write your code here
+def countdown(int)
+  while int > 0 
+  puts "#{int} SECOND(S)!"
+  int -= 1
+  end
+ "HAPPY NEW YEAR!"
+end
 
-def countdown
+# countdown(10)
+
+def countdown_with_sleep(int)
+  while int > 0 
+  puts "#{int} SECOND(S)!"
+  int -= 1
+  sleep 1
+  end
+ "HAPPY NEW YEAR!"
 end
