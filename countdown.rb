8e0fa9e2fd290ca66386388ff7ee00df48ce3da9
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-06-18 08:38:44 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,5 @@
 def countdown(int)
+<<<<<<< HEAD
   while int > 0 
   puts "#{int} SECOND(S)!"
   int -= 1
@@ -15,4 +16,11 @@ def countdown_with_sleep(int)
   sleep 1
   end
  "HAPPY NEW YEAR!"
+=======
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep
+  sleep(5)
+>>>>>>> 312168f61b5d0d469263e056f0d4a176108dba67
 end
